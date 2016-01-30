25f17540ee50a0a32d215d17c73057ced52c4f97
Aaron Wilson <aaronnw11@gmail.com>
2016-01-29 20:43:38 -0600

    Reset

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,19 +1,7 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
-void printThis(int x, int y) {
-	cout << "x = " << x << " and y = " << y << endl;
+int main(){
+	
 }
-int main() {
-	int x, y;
-	cin >> x;
-	cin >> y;
-	printThis(x, y);
-}
-
-
-//TODO
-//Read input file
-//Create array of actions
-//Print each action along with its number and result
-//
+			
