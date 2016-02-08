94cb1421ff085aba75011590df79cf4b9a8b1d19
Aaron Wilson <aaronnw11@gmail.com>
2016-02-08 04:29:18 -0600

    Comments and cleanup

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
-
+///Sets all characters in a string of a given length to \0
 void strEmpty(char* someStr, int length) {
 	for (int i = 0; i < length; i++) {
 		someStr[i] = '\0';
 	}
 }
+///Class for web address information
 class webAddressInfo {
 private:
 	char url[201]; //allow a maximum of 200 characters
@@ -16,7 +17,7 @@ public:
 	char* getWebAddressInfo();
 	void display();
 };
-
+///Class for each browser tab
 class browserTab {
 protected:
 	int numAddress; //Current number of web addresses in this tab
@@ -32,49 +33,52 @@ public:
 	void display();
 	// and other public methods if necessary
 };
-
+///Default constructor
 webAddressInfo::webAddressInfo() {
 }
-
+///Initializer
 webAddressInfo::webAddressInfo(char* inputString) {
 	int i = 0;
 	strEmpty(url, 201);
+	//Make a deep copy from the input string to the url
 	while (inputString[i] != '\0') {
 		url[i] = inputString[i];
 		i++;
 	}
 }
-
+///Sets the url to a given string
 void webAddressInfo::setWebAddressInfo(char * inputString) {
 	int i = 0;
 	while (inputString[i] != '\0') {
 		url[i] = inputString[i];
 	}
 }
-
+///Returns the character array for the web address
 char* webAddressInfo::getWebAddressInfo() {
 	return url;
 }
-
+///Displays the information for a single web address
 void webAddressInfo::display() {
 	int i = 0;
+	//Print out each valid character in the url
 	while (url[i] != '\0') {
 		cout << url[i];
 		i++;
 	}
 }
-
+///Default constructor
 browserTab::browserTab() {
 	numAddress = 0;
 }
-
+///Initializer
 browserTab::browserTab(char* inputString) {
 	numAddress = 1;
 	webAddresses[numAddress - 1] = webAddressInfo(inputString);
 	currentAddress = numAddress - 1;
 }
-
+///Returns the information for an address forward from the current
 webAddressInfo& browserTab::forward() {
+	//If told to go forward from the last address, just return the current
 	if ((currentAddress + 1) < numAddress) {
 		return webAddresses[currentAddress + 1];
 	}
@@ -82,8 +86,9 @@ webAddressInfo& browserTab::forward() {
 		return webAddresses[currentAddress];
 	}
 }
-
+///Returns the information for an address backward from the current
 webAddressInfo& browserTab::backward() {
+	//If told to go backward from the first address, just return the current
 	if (currentAddress > 0) {
 		return webAddresses[currentAddress - 1];
 	}
@@ -91,7 +96,7 @@ webAddressInfo& browserTab::backward() {
 		return webAddresses[currentAddress];
 	}
 }
-
+///Adds a new address to a tab
 void browserTab::addAddress(char* inputString) {
 	numAddress++;
 	webAddresses[numAddress - 1] = webAddressInfo(inputString);
@@ -99,13 +104,13 @@ void browserTab::addAddress(char* inputString) {
 
 
 }
-
+///Displays all the addresses contained in a tab
 void browserTab::display() {
 	for (int i = 0; i < numAddress; i++) {
 		webAddresses[i].display();
 	}
 }
-
+///Main method
 int main() {
 	browserTab myTabs[20];
 	int tabNumber = 0;
@@ -116,18 +121,18 @@ int main() {
 	char action;
 
 	while (!cin.eof()) { // while end of file is not reached
-
+		//Read in the first characters
 		cin >> tabNumber;
 		cin.get(blank);
 		cin.get(action);
 		strEmpty(webAddress, 201);
 		switch (action) {
+		//Create a new address information object with the url and add it to a tab
 		case 'N': {
 			i = 0;
 			do {
 				cin.get(c);
 				if (c != '\n') {
-					webAddress[i++] = c;
 				}
 			} while ((c != '\n') && (i < 201) && !cin.eof());
 			new webAddressInfo(webAddress);
@@ -135,7 +140,7 @@ int main() {
 			cout << tabNumber << " " << action << " " << webAddress;
 			break;
 		}
-
+		//Move to the next address in the tab and display the url
 		case 'F': {
 			if (!cin.eof()) {
 				cout << tabNumber << " " << action << " ";
@@ -143,7 +148,7 @@ int main() {
 			}
 			break;
 		}
-
+		//Move to the previous address in the tab and display the url
 		case 'B': {
 			if (!cin.eof()) {
 				cout << tabNumber << " " << action << " ";
@@ -152,7 +157,7 @@ int main() {
 			}
 			break;
 		}
-
+		//Print all the address information in the tab
 		case 'P': {
 			if (!cin.eof()) {
 				cout << tabNumber << " " << action << " ";
